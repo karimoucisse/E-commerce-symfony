b93d85a5bf4f27612959868f28f0631f7f688b3c
karimou cisse <karimou@pylote.io>
2023-01-26 17:46:01 +0100

    [ADD] categorie and produit entity

---
+++ src/Form/ProduitType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produit;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('description')
+            ->add('prix')
+            ->add('stock')
+            ->add('image', FileType::class, [
+                'label' => 'Image (jpg, png, jpeg)',
+                'mapped' => false,
+                'required' => true,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/png',
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'seul les jpg, pnp et jpeg sont autorisés',
+                    ])
+                ],
+            ])
+            ->add('categorie')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
